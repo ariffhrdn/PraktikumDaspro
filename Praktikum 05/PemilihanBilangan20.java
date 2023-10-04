79b828502ec99a752232491b57a27d3d070d3eda
unknown <pickarly@gmail.com>
2023-10-04 11:18:06 +0700

    Ubah 10

---
+++ Praktikum 05/PemilihanBilangan20.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PemilihanBilangan20{
+      public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Sebuah Angka: ");
+        int angka = sc.nextInt();
+
+        if{}
+
+    
+    }
+}
