bf18aad3ba24ebdf2528ec970f084e5b66c40ce2
unknown <pickarly@gmail.com>
2023-10-11 11:34:43 +0700

    Perubahan lagi

--- Praktikum06/KabisatNested20.java
+++ Praktikum06/KabisatNested20.java
@@ -10,7 +10,10 @@ public class KabisatNested20 {
 
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
-                System.out.println("Tahun Kabisat: ");
+                System.out.println("Tahun Kabisat");
+
+            else 
+                System.out.println("Bukan Tahun Kabisat");    
         } else 
             System.out.println("Bukan Tahun Kabisat");
         
