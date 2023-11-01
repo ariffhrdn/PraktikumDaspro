96928bb2f04861c01f5f23bf5aacdba2917c4fff
unknown <pickarly@gmail.com>
2023-11-01 13:33:36 +0700

    Praktikum 9 Percobaan 2 Pertanyaan terakhir, menambahkan rata rata, tertinggi dan terendah, jumlah kelulusan

--- Praktikum09/ArrayNilai20.java
+++ Praktikum09/ArrayNilai20.java
@@ -5,19 +5,39 @@ public class ArrayNilai20 {
         
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int [10];
+        int total = 0, tertinggi =0, terendah =100,jumlahLulus =0, jumlahTidakLulus =0;
+        double rata;
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir mahasiswa ke- " + (i+1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
+
+        if (nilaiAkhir[i] < terendah) {
+            terendah = nilaiAkhir[i];
+        } 
+        else if (nilaiAkhir[i] > tertinggi) {
+            tertinggi = nilaiAkhir[i];
+        }  
+        total += nilaiAkhir[i]; 
         }
+        
 
         for (int i = 0; i < 10; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Mahasiswa ke- " + (i+1) + " lulus " ); 
+                jumlahLulus++;
             }
             else {
                 System.out.println("Mahasiswa ke- " + (i+1) + " tidak lulus " );
+                jumlahTidakLulus++;
             }
         }
+        rata = (double)total / nilaiAkhir.length;
+        
+        System.out.println("Nilai rata rata adalah: " + rata);
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah adalah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " +jumlahLulus);
+        System.out.println("Jumlah Mahasiswa Lulus: " +jumlahTidakLulus);
     }
 }
