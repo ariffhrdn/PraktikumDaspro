25f6b0f3b4342792bd385b9d6cd3b8c558f4c18f
unknown <pickarly@gmail.com>
2023-11-01 14:58:00 +0700

    Praktikum 9 Percobaan 3 Pertanyaan ke 3

--- Praktikum09/LinearSearch20.java
+++ Praktikum09/LinearSearch20.java
@@ -1,18 +1,35 @@
 package Praktikum09;
-
+import java.util.Scanner;
 public class LinearSearch20 {
     public static void main(String[] args) {
         
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        int key, elemen;
+        int hasil = -1;
+        
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        elemen = sc.nextInt();
+        int arrayInt[] = new int[elemen];
 
         for (int i =0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke- " + (i) + " : " );
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
-                break;
+                System.out.println("Key ada di posisi indeks ke- " + hasil);
+                
             }
         }
-        System.out.println("Key ada dalam array pada posisi index ke- " + hasil);
+         if (hasil == -1){
+                System.out.println("Key tidak ditemukan");
+                
+            }
     }
 }
