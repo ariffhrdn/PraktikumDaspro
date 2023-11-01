7a03522288d1899d9f6ed28a94db2b415d1f4b85
unknown <pickarly@gmail.com>
2023-11-01 11:53:20 +0700

    Praktikum 09 Percobaan 2 Pertanyaan

--- Praktikum09/ArrayNilai20.java
+++ Praktikum09/ArrayNilai20.java
@@ -6,13 +6,18 @@ public class ArrayNilai20 {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int [10];
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan nilai akhir makasiswa ke- " + (i+1) + ": ");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir mahasiswa ke- " + (i+1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir mahasiswa ke- " + (i+1) + " adalah " + nilaiAkhir[i] );
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke- " + (i+1) + " lulus " ); 
+            }
+            else {
+                System.out.println("Mahasiswa ke- " + (i+1) + " tidak lulus " );
+            }
         }
     }
 }
