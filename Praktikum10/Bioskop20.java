ff3018ad6b33e63337ceee0f40dcbe63132b283f
unknown <pickarly@gmail.com>
2023-11-08 11:49:59 +0700

    penggunaan foreach untuk menampilkan baris

--- Praktikum10/Bioskop20.java
+++ Praktikum10/Bioskop20.java
@@ -12,7 +12,7 @@ public class Bioskop20 {
         penonton [2][0] = "Eka";
         penonton [2][1] = "Farhan";
         penonton [3][0] = "Gisel";
-        penonton [3][0] = "Hana";
+        penonton [3][1] = "Hana";
 
         System.out.printf("%s \t %s \n", penonton [0][0], penonton [0][1]);
         System.out.printf("%s \t %s \n", penonton [1][0], penonton [1][1]);
@@ -21,8 +21,8 @@ public class Bioskop20 {
 
         System.out.println(penonton.length);
       
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke- " + (i+1) + ": " + penonton[i].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
     }
 }
