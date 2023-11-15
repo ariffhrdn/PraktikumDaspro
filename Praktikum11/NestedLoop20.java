3a4c698fcc82f9b4e4d2b71eb2b65a377b703f24
unknown <pickarly@gmail.com>
2023-11-15 13:39:01 +0700

    menambahkan input banyaknya array, memberi rata rata dan menambahkan foreach untuk menampilkan suhu

--- Praktikum11/NestedLoop20.java
+++ Praktikum11/NestedLoop20.java
@@ -4,7 +4,13 @@ public class NestedLoop20 {
         
         Scanner sc = new Scanner(System.in);
 
-        double[][] suhu = new double[2][4];
+        int kota;
+        double rata = 0;
+
+        System.out.print("Masukkan Jumlah Kota yang Ingin Disimpan: ");
+        kota = sc.nextInt();
+
+       double[][] suhu = new double[kota][4];
 
         for (int i = 0; i < suhu.length; i++) {
             System.out.println("KOTA KE- " + (i+1));
@@ -19,11 +25,24 @@ public class NestedLoop20 {
         for (int i = 0; i < suhu.length; i++) {
             System.out.println("KOTA KE- " + (i+1));
 
-            for (int j = 0; j < suhu[i].length; j++) {
-                System.out.print(suhu[i][j] + " ");
-               
+            for (double temp : suhu[i]) {
+                System.out.println(temp);
+                
             }
-            System.out.println();
+           System.out.println();
         }
+
+        
+        for (int i = 0; i < suhu.length; i++) {
+            System.out.print("Rata rata suhu kota ke- " + (i+1) + "adalah: ");
+            double total = 0;
+            for (int j = 0; j < suhu[i].length; j++) {
+                total = total + suhu[i][j];
+            }
+            rata =(double)total / suhu[i].length;
+            System.out.println(rata);
+        }
+sc.close();
     }
+    
 }
