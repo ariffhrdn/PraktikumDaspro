356abe6333537fe0a53dfd754e3685b8b1534318
unknown <pickarly@gmail.com>
2023-11-29 11:44:13 +0700

    feat: menggunakan fungsi pada kode program

---
+++ Praktikum13/Welcome20.java
@@ -0,0 +1,11 @@
+package Praktikum13;
+
+public class Welcome20 {
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+    
+    public static void displayWelcomeMessage() {
+        System.out.println("Selamat Datang di Politeknik Negeri Malang");
+    }
+}
---
+++ Praktikum13/Welcome220.java
@@ -0,0 +1,14 @@
+package Praktikum13;
+
+public class Welcome220 {
+    public static void main(String[] args) {
+        displayWecomeMessage("Ani");
+        displayWecomeMessage("Budi");
+        displayWecomeMessage("Cica");
+        displayWecomeMessage("Dodi");
+    }
+
+    public static void displayWecomeMessage(String name) {
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+}    
+}
